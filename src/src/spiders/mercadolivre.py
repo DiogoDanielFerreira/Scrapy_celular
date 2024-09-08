ed56f97a5a4581d84629201267b3628b02dfbf9d
Diogo Daniel <diogodaniel630@gmail.com>
2024-09-08 18:47:28 -0300

    corrigindo erros do scraping

--- src/src/spiders/mercadolivre.py
+++ src/src/spiders/mercadolivre.py
@@ -4,8 +4,8 @@ import scrapy
 class MercadolivreSpider(scrapy.Spider):
     name = "mercadolivre"
     start_urls = ["https://lista.mercadolivre.com.br/celular"]
-    page_count = 1
-    max_pages = 10
+  #  page_count = 1
+  #  max_pages = 10
 
     def parse(self, response):
 
@@ -23,15 +23,15 @@ class MercadolivreSpider(scrapy.Spider):
                 'brand': product.css('p.ui-search-official-store-label.ui-search-item__group__element.ui-search-color--GRAY::text').get(),
                 'name': product.css('h2.ui-search-item__title::text').get(),
                 'old_price_reais': prices[0] if len(prices) > 0 else None,
-                'old_price_cents': cents[0] if len(prices) > 0 else None,
+                'old_price_cents': cents[0] if len(cents) > 0 else None,
                 'new_price_reais': prices[1] if len(prices) > 1 else None,
-                'new_price_cents': cents[1] if len(prices) > 1 else None
+                'new_price_cents': cents[1] if len(cents) > 1 else None,
 
             }
 
-        if self.page_count < self.max_pages:
-            next_page = response.css(
-                'li.andes-pagination__button.andes-pagination__button--next a::attr(href)').get()
-            if next_page:
-                self.page_count += 1
-                yield scrapy.Request(url=next_page, callback=self.parse)
+#        if self.page_count < self.max_pages:
+ #           next_page = response.css(
+  #              'li.andes-pagination__button.andes-pagination__button--next a::attr(href)').get()
+   #         if next_page:
+    #            self.page_count += 1
+     #           yield scrapy.Request(url=next_page, callback=self.parse)
